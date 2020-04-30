e055fe6053da8844dd933cce9217b287d5ea2bd2
rozz <jack.and.rozz@gmail.com>
2020-05-01 00:44:30 +0900

    add a script to remove unused picures

---
+++ scripts/remove_unannotated_pics.py
@@ -0,0 +1,15 @@
+import glob, os, argparse
+
+def main(args):
+    paths = list(glob.glob(args.target_dir + '/*.png')) + list(glob.glob(args.target_dir + '/*.jpg'))
+    for path in paths:
+        header = '.'.join(path.split('.')[:-1])
+        # print(os.path.exists(header + '.xml'))
+        if not os.path.exists(header + '.xml'):
+            os.remove(path)
+
+if __name__ == "__main__":
+      parser = argparse.ArgumentParser()
+      parser.add_argument('--target-dir', default='.')
+      args = parser.parse_args()
+      main(args)
